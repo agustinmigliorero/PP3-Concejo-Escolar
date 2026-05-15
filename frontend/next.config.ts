78d8c60dfbe7c8792b7392aa994918d2632fd767
Agustin Migliorero Tumini <56460586+agustinmigliorero@users.noreply.github.com>
2026-05-15 20:45:18 -0300

    Solucionado el turbopack

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["92.113.39.212"],
-  turbopack: {
-    root: path.resolve(__dirname),
-  },
 };
 
 export default nextConfig;
